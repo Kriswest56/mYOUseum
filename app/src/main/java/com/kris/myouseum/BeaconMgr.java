05354dbfb7ba00a9dee9ba2302f2340595c6a52e
Gideon <gnyamach@msudenver.edu>
2017-04-23 16:02:03 -0600

    Added the quick start discoverable code to Main.

--- app/src/main/java/com/kris/myouseum/BeaconMgr.java
+++ app/src/main/java/com/kris/myouseum/BeaconMgr.java
@@ -2,7 +2,9 @@ package com.kris.myouseum;
 
 import android.app.Application;
 
+import com.estimote.coresdk.observation.region.Region;
 import com.estimote.coresdk.service.BeaconManager;
+import java.util.UUID;
 
 /**
  * Created by gnyama200 on 4/23/2017.
@@ -15,6 +17,15 @@ public class BeaconMgr extends Application{
     public void onCreate(){
         super.onCreate();
         beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+               // beaconManager.startMonitoring(new Region(
+                 //       "monitored region",
+                  //      UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                    //    22504, 48827));
+            }
+        });
     }
 
 }
--- app/src/main/java/com/kris/myouseum/MainActivity.java
+++ app/src/main/java/com/kris/myouseum/MainActivity.java
@@ -4,13 +4,21 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 import butterknife.ButterKnife;
+
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.recognition.packets.Nearable;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private BeaconManager beaconManager;
+    private  String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        beaconManager = new BeaconManager(this);
 
+        beaconManager.setNearableListener(new BeaconManager.NearableListener() {
+                @Override
+                public void onNearablesDiscovered(List<Nearable> nearables) {
+                   Log.e(TAG, "Discovered Nearables: " + nearables);
+                }
+            }
+
+        );
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startNearableDiscovery();
+            }
+        });
     }
 
     @Override
@@ -33,5 +62,17 @@ public class MainActivity extends AppCompatActivity {
         //this checks dependencies
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        beaconManager.startNearableDiscovery();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.disconnect();
     }
 }
