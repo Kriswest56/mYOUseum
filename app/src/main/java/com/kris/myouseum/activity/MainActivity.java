e1a760e1fd226797035f5c36bec77c0a68a1f543
Gideon <gnyamach@msudenver.edu>
2017-05-03 18:06:11 -0600

    Added the coarse permissions and resized images

--- app/src/main/java/com/kris/myouseum/activity/MainActivity.java
+++ app/src/main/java/com/kris/myouseum/activity/MainActivity.java
@@ -3,7 +3,10 @@ package com.kris.myouseum.activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
 
 import butterknife.ButterKnife;
 
@@ -14,11 +17,13 @@ import com.kris.myouseum.fragment.HomeScreenFragment;
 import com.kris.myouseum.utils.Constants;
 import com.kris.myouseum.utils.ExhibitInterface;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
 public class MainActivity extends Activity implements ExhibitInterface {
 
 
     private String TAG = MainActivity.class.getSimpleName();
-
+    private static final int REQUEST_COARSE_LOCATION_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class MainActivity extends Activity implements ExhibitInterface {
 
         ft.commit();
 
+        doDiscovery();
         ButterKnife.bind(this);
 
     }
@@ -53,6 +59,18 @@ public class MainActivity extends Activity implements ExhibitInterface {
         ft.replace(R.id.fragment_frame, frag);
         ft.commit();
 
+    }
+
+    private void doDiscovery() {
+        android.util.Log.e(TAG, "/t/t doDiscovery() : needs explicit acceptance or coarse");
+        int hasPermission = ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION);
+
+        if (!(hasPermission == PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_COARSE_LOCATION_PERMISSIONS);
+
+        }
     }
 
 }
